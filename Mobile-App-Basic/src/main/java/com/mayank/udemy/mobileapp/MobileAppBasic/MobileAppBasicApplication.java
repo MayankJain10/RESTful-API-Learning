74a9454f0dbe24a175f75bfde0c4fafe59cb4632
Mayank Jain <73280608+MayankJain10@users.noreply.github.com>
2023-07-01 18:59:57 +0530

    Learning
    initial commit

---
+++ Mobile-App-Basic/src/main/java/com/mayank/udemy/mobileapp/MobileAppBasic/MobileAppBasicApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.udemy.mobileapp.MobileAppBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppBasicApplication.class, args);
+	}
+
+}
---
+++ Mobile-App-Basic/src/test/java/com/mayank/udemy/mobileapp/MobileAppBasic/MobileAppBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.udemy.mobileapp.MobileAppBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
