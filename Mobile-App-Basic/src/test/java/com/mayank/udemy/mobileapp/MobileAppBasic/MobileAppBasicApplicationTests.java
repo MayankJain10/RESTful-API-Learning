40d8ad921f60fa4c89afa673fac318c1f5ca3662
MayankJain-aq <Mayank.Jain@airlinq.com>
2023-07-08 14:56:08 +0530

    ANNOTATIONS LEARNING

--- Mobile-App-Basic/src/test/java/com/mayank/udemy/mobileapp/MobileAppBasic/MobileAppBasicApplicationTests.java
+++ Mobile-App-Basic/src/test/java/com/mayank/udemy/mobileapp/MobileAppBasic/MobileAppBasicApplicationTests.java
@@ -1,6 +1,6 @@
 package com.mayank.udemy.mobileapp.MobileAppBasic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
